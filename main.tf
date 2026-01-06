c09e85ece98416160c0049699d4913c4bff88132
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2026-01-06 16:02:13 +0000

    fix
    Signed-off-by: Mateus Caruccio <mateus.caruccio@getupcloud.com>

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
 locals {
   public_info_read = <<-EOF
-      timeout -k $((TIMEOUT + 2)) $TIMEOUT bash -c '
-        set -o pipefail
-        if ip=$(curl -sk --connect-timeout $TIMEOUT $PUBLIC_IP_DISCOVERY_ENDPOINT_IFCONFIG_ME 2>/dev/null); then
-          echo '{"public_ip": "'$ip'","cidr_block": "'$ip/32'","source":"'$PUBLIC_IP_DISCOVERY_ENDPOINT_IFCONFIG_ME'"}'
-        elif ip=$(curl -sk --connect-timeout $TIMEOUT $PUBLIC_IP_DISCOVERY_ENDPOINT_IPINFO_IO 2>/dev/null | jq -r .ip); then
-          echo '{"public_ip": "'$ip'","cidr_block": "'$ip/32'","source":"'$PUBLIC_IP_DISCOVERY_ENDPOINT_IPINFO_IO'"}'
-        else
-          echo '{"public_ip": "0.0.0.0","cidr_block": "0.0.0.0/32","source":null}'
-        fi
-    '
-    EOF
+    set -o pipefail
+    if ip=$(curl -sk --connect-timeout $TIMEOUT $PUBLIC_IP_DISCOVERY_ENDPOINT_IFCONFIG_ME 2>/dev/null); then
+      echo '{"public_ip": "'$ip'","cidr_block": "'$ip/32'","source":"'$PUBLIC_IP_DISCOVERY_ENDPOINT_IFCONFIG_ME'"}'
+    elif ip=$(curl -sk --connect-timeout $TIMEOUT $PUBLIC_IP_DISCOVERY_ENDPOINT_IPINFO_IO 2>/dev/null | jq -r .ip); then
+      echo '{"public_ip": "'$ip'","cidr_block": "'$ip/32'","source":"'$PUBLIC_IP_DISCOVERY_ENDPOINT_IPINFO_IO'"}'
+    else
+      echo '{"public_ip": "0.0.0.0","cidr_block": "0.0.0.0/32","source":null}'
+    fi
+  EOF
 }
 
 resource "shell_script" "public_info" {
